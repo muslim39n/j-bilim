94e017ba0cff89ab9192e38a79b06219d3bd049c
HanTengri <muslim39n@gmail.com>
2022-05-12 09:46:41 +0600

    Forum comment

--- jbilimproject/forum/urls.py
+++ jbilimproject/forum/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ForumPageList, ForumPageDetail
+from .views import ForumPageList, ForumPageDetail, CommentPostView
 
 urlpatterns = [
     path('', ForumPageList.as_view(), name='forum_all_pages'),
     path('<int:pk>/', ForumPageDetail.as_view(), name='forum_page_detail'),
+    path('<int:page_id>/comment/', CommentPostView.as_view(), name='add_comment'),
 ]
--- jbilimproject/forum/views.py
+++ jbilimproject/forum/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from rest_framework.status import HTTP_201_CREATED
 
@@ -17,9 +18,20 @@ class ForumPagePagination(PageNumberPagination):
 
 
 class ForumPageList(ListAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
     queryset = ForumPage.objects.all()
     serializer_class = ForumPageShortSerializer
     pagination_class = ForumPagePagination
+
+    def post(self, request, format=None):
+        page = ForumPage(title=request.data['title'],
+                        body=request.data['body'],
+                        user=request.user)
+
+        page.save()
+
+        return Response(ForumPageSerializer(page).data, status=HTTP_201_CREATED) 
+        
         
 class ForumPageDetail(APIView):
     def get_object(self, pk):
@@ -32,41 +44,27 @@ class ForumPageDetail(APIView):
         page = self.get_object(pk)
         serializer = ForumPageSerializer(page)
         return Response(serializer.data)
-    
-'''
-class PostList(APIView):
-    def get(self, request, format=None):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        if 'added' in request.data:
-            post = Post.objects.create(title=request.data['title'],
-                                        added=request.data['added'],
-                                        original_url=request.data['original_url'])
-
-        else:
-            post = Post.objects.create(title=request.data['title'],
-                                        original_url=request.data['original_url'])
-
-        for p_text in request.data['paragraphs']:
-            Paragraph.objects.create(post=post, text=p_text)
-
-        return Response(PostSerializer(post).data, status=HTTP_201_CREATED)
 
 
-class PostDetail(APIView):
-    def get_object(self, pk):
+class CommentPostView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    def post(self, request, page_id, format=None):
         try:
-            return Post.objects.get(pk=pk)
+            page = ForumPage.objects.get(pk=page_id)
         except:
             raise Http404
 
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-
-        serializer = PostSerializer(post)
-
-        return Response(serializer.data)
         '''
+        forum_page = models.ForeignKey(ForumPage, on_delete=models.CASCADE)
+        body = models.TextField()
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        
+        created = models.DateTimeField(auto_now_add=True)
+        '''
+        comment = Comment(forum_page=page,
+                        body=request.data['body'],
+                        user=request.user)
+
+        comment.save()
+        
+        return Response(ForumPageSerializer(page).data, status=HTTP_201_CREATED) 
